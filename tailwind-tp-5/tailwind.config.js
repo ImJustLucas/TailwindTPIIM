60eb893ecb67afd9303eec4af8d25e4aaa859875
Lucas <bellierlucas.pro@gmail.com>
2023-11-21 16:31:14 +0100

    Add Tailwind CSS configuration and styles

---
+++ tailwind-tp-5/tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        roboto: ["Roboto", "sans-serif"],
+      },
+      colors: {
+        "bg-light": "#f7f7f7",
+        "": "",
+        "": "",
+        "": "",
+      },
+    },
+  },
+  plugins: [],
+  darkMode: "class",
+};
